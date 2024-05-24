e411bd62f6cc68710b71a21c0e459dd965e1e6c7
heahl <hendrik712.a@gmail.com>
2024-05-24 11:13:25 +0200

    final commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,15 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        'violet': 'hsl(257, 40%, 49%)',
+        'soft-magenta': 'hsl(300, 69%, 71%)',
+      },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        poppinsLight: ["Poppins Light", "sans-serif"],
+        poppinsMedium: ["Poppins Medium", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"],
       },
     },
   },
